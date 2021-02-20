7bf516b1c6c5af76e09414b86f64de1a5651a0e5
Casey Law <caseyjlaw@gmail.com>
2021-02-20 06:32:22 -0800

    setuptools working

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from version import get_git_version
 setup(name='vlass_tools',
       version=get_git_version(),
       url='http://github.com/caseyjlaw/vlass',
-      packages=['vlass'],
+      packages=['vlass_tools'],
       requirements=['astropy', 'numpy'],
       zip_safe=False)
